7657f81503e3cba215c5256ce80840d79ab84509
Merlin <1113739263@qq.com>
2017-08-09 20:43:56 +0800

    注册事件完成了

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    document.querySelector("box").onclick=function(){
+        alert("我是一个盒子啦");
+    }
+    document.querySelector("p").onclick=function(){
+        alert("我是一个段落啦");
+    }
+}
